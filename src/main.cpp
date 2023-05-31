706b3b082d0455344baefe66d11dcac7e55b8b13
CHEN Paul <paul.chen@geii2.iut-cachan.u-psud.fr>
2023-05-31 17:34:41 +0200

    Added test of Output Input

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,28 @@
 ThreadLvgl threadLvgl(30);
 // Initialize a pins to perform analog input and digital output functions
 AnalogIn   capteur1(A0), capteur2(A1);
-DigitalOut pompe1(D1), pompe2(D2);
+PwmOut pompe1(D11), pompe2(D3);
+double t;
 
 int main() {
 
     threadLvgl.lock();
 
-    printf("Valeur Capteur1 : %d", capteur1);
-    printf("Valeur Capteur2 : %d", capteur2);
-    
-    lv_demo_widgets();
+    pompe1.period_us(50);
+    pompe2.period_us(50);
+    pompe1.write(0.50f);
+    pompe2.write(0.50f);
+
 
     threadLvgl.unlock();
 
     while (1) {
         // put your main code here, to run repeatedly:
-        ThisThread::sleep_for(10ms);
+        ThisThread::sleep_for(500ms);
+
+            printf("\nValeur Capteur 1 : %f\n", capteur1.read());
+            printf("\nValeur Capteur 2 : %f\n", capteur2.read());
+
+
     }
 }
