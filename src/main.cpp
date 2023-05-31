6e474624dc9f57ee3e3114e1963a398953b3a5fb
CHEN Paul <paul.chen@geii2.iut-cachan.u-psud.fr>
2023-05-31 13:46:56 +0200

    Added Input/Output

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,17 @@
 #include <cstdio>
 
 ThreadLvgl threadLvgl(30);
+// Initialize a pins to perform analog input and digital output functions
+AnalogIn   capteur1(A0), capteur2(A1);
+DigitalOut pompe1(D1), pompe2(D2);
 
 int main() {
 
     threadLvgl.lock();
 
+    printf("Valeur Capteur1 : %d", capteur1);
+    printf("Valeur Capteur2 : %d", capteur2);
+    
     lv_demo_widgets();
 
     threadLvgl.unlock();
